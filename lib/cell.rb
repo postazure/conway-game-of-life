62afcd7db921d377564c656fff573de12f4bcaeb
Max Gordon <gordonmaxc@gmail.com>
2014-12-25 14:01:07 -0800

    next generation spec complete

--- lib/cell.rb
+++ lib/cell.rb
@@ -8,18 +8,42 @@ class Cell
   end
 
   def new_generation(neighbors)
-    living_neighbors = neighbors.map {|x| x.living == true}
-
+    living_neighbors = neighbors.select {|x| x.living == true}
+    # puts "#"*20
+    # p living_neighbors
+    # puts living_neighbors.count
     if self.living
       if living_neighbors.count < 2
         return Cell.new(false)
       elsif living_neighbors.count > 3
         return Cell.new(false)
-      end
-    else
-      if living_neighbors.count == 3
+      else
         return Cell.new(true)
       end
+    else
+      # puts "#"*20
+      # puts "Cell is Dead"
+      if living_neighbors.count == 3
+      #   puts "Cell has 3 living neighbors"
+        return Cell.new(true)
+      else
+        return Cell.new(false)
+      end
     end
+    # if self.living
+    #   if living_neighbors.count < 2
+    #     return Cell.new(false)
+    #   elsif living_neighbors.count > 3
+    #     return Cell.new(false)
+    #   else
+    #     return Cell.new(true)
+    #   end
+    # else
+    #   if living_neighbors.count == 3
+    #     return Cell.new(true)
+    #   else
+    #     return Cell.new(false)
+    #   end
+    # end
   end
 end
--- spec/new_generation_spec.rb
+++ spec/new_generation_spec.rb
@@ -14,4 +14,80 @@ describe "new generation" do
     expect(cell1.living).to be true
     expect(cell2.living).to be false
   end
+
+  describe "Any live cell with fewer than two live neighbours dies, as if caused by under-population" do
+    it "has 1 living neighbor" do
+      test_board = Board.new(2)
+      test_board.genesis_grid(Cell)
+      test_board.seed_grid([0,0])
+      test_board.seed_grid([1,1])
+      test_board.next_gen_board
+      test_cell = test_board.cell([1,1])
+
+      expect(test_cell.living).to be false
+    end
+  end
+
+  describe "Any live cell with two or three live neighbours lives on to the next generation" do
+    it "has 2 living neighbor" do
+      test_board = Board.new(2)
+      test_board.genesis_grid(Cell)
+      test_board.seed_grid([0,0])
+      test_board.seed_grid([1,0])
+      test_board.seed_grid([0,1])
+
+      test_board.next_gen_board
+      test_cell = test_board.cell([0,1])
+
+      expect(test_cell.living).to be true
+    end
+
+    it "has 3 living neighbor" do
+      test_board = Board.new(2)
+      test_board.genesis_grid(Cell)
+      test_board.seed_grid([0,0])
+      test_board.seed_grid([1,0])
+      test_board.seed_grid([0,1])
+      test_board.seed_grid([1,1])
+
+      test_board.next_gen_board
+      test_cell = test_board.cell([0,1])
+
+      expect(test_cell.living).to be true
+    end
+  end
+
+  describe "Any live cell with more than three live neighbours dies, as if by overcrowding" do
+    it "has 4 living neighbor" do
+      test_board = Board.new(3)
+      test_board.genesis_grid(Cell)
+      test_board.seed_grid([0,0])
+      test_board.seed_grid([1,0])
+      test_board.seed_grid([2,0])
+      test_board.seed_grid([0,1])
+      test_board.seed_grid([1,1])
+
+      test_board.next_gen_board
+      test_cell = test_board.cell([1,1])
+
+      expect(test_cell.living).to be false
+    end
+  end
+
+  describe "Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction" do
+    it "has 3 living neighbor" do
+      test_board = Board.new(3)
+      test_board.genesis_grid(Cell)
+
+      test_board.seed_grid([0,0])
+      test_board.seed_grid([0,1])
+      test_board.seed_grid([0,2])
+      test_board.next_gen_board
+
+      test_cell = test_board.cell([1,1])
+
+      expect(test_cell.living).to be true
+    end
+
+  end
 end
